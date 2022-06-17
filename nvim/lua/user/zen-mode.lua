0f8f0803b9929d1e744f0830ace7fa37d7fb6e04
Shivan Sivakumarnan <51132467+ShivanS93@users.noreply.github.com>
2022-06-18 09:30:06 +1200

    editing dotfiles

--- nvim/lua/user/zen-mode.lua
+++ nvim/lua/user/zen-mode.lua
@@ -21,7 +21,7 @@ zen_mode.setup({
 		-- uncomment any of the options below, or add other vim.wo options you want to apply
 		options = {
 			signcolumn = "no", -- disable signcolumn
-			number = false, -- disable number column
+			number = true, -- disable number column
 			relativenumber = false, -- disable relative numbers
 			wrap = true, -- wrap
 			linebreak = true, -- wrap words
@@ -41,7 +41,7 @@ zen_mode.setup({
 		},
 		twilight = { enabled = true }, -- enable to start Twilight when zen mode opens
 		gitsigns = { enabled = false }, -- disables git signs
-		tmux = { enabled = false }, -- disables the tmux statusline
+		tmux = { enabled = true }, -- disables the tmux statusline
 		-- this will change the font size on kitty when in zen mode
 		-- to make this work, you need to set the following kitty options:
 		-- - allow_remote_control socket-only
