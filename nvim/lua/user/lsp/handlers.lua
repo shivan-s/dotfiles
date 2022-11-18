4b9ad4b51ae2a4aca40109635d485171efd5441c
Shivan Sivakumaran <51132467+ShivanS93@users.noreply.github.com>
2022-11-19 11:04:34 +1300

    fix: fixing formatting

--- nvim/lua/user/lsp/handlers.lua
+++ nvim/lua/user/lsp/handlers.lua
@@ -79,8 +79,8 @@ local function lsp_keymaps(bufnr)
 	)
 	vim.api.nvim_buf_set_keymap(bufnr, "n", "]d", '<cmd>lua vim.diagnostic.goto_next({ border = "rounded" })<CR>', opts)
 	vim.api.nvim_buf_set_keymap(bufnr, "n", "<leader>q", "<cmd>lua vim.diagnostic.setloclist()<CR>", opts)
-	vim.cmd([[ command! Format :lua vim.lsp.buf.formatting() ]])
-	vim.cmd([[autocmd BufWritePre <buffer> lua vim.lsp.buf.format()]])
+	vim.cmd([[ command! Format :lua vim.lsp.buf.format({ async = true }) ]])
+	vim.cmd([[autocmd BufWritePre <buffer> lua vim.lsp.buf.format({ async = true })]])
 end
 
 M.on_attach = function(client, bufnr)
