73039346218afc6f1860cb8bf9f10e3cde46b05d
Shivan Sivakumaran <51132467+ShivanS93@users.noreply.github.com>
2023-10-05 22:56:48 +1300

    updating zk

---
+++ move.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+ln -sf $(pwd)/nvim $HOME/.config/nvim
+ln -sf $(pwd)/fish $HOME/.config/fish
+ln -sf $(pwd)/omf $HOME/.config/omf
+ln -sf $(pwd)/.tmux.conf $HOME/.tmux.conf
+ln -sf $(pwd)/.tmux.conf.local $HOME/.tmux.conf.local
+ln -sf $(pwd)/sql-language-server $HOME/.config/sql-language-server
+
+mkdir $HOME/.zk
+ln -sf $(pwd)/zk/templates $HOME/.zk/templates
+ln -sf $(pwd)/zk/.config $HOME/.zk/.config
+
---
+++ zk/templates/standup.md
@@ -0,0 +1,25 @@
+# {{ format-date now "long" }}
+
+---
+
+## Working on Yesterday
+
+- ***
+
+---
+
+## Working on Today
+
+- ***
+
+---
+
+## Blocked on
+
+- ***
+
+---
+
+## Notes
+
+- ***
