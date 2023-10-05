131f693f2104839b5f77b077e821ba9f4cc8edab
Shivan Sivakumaran <51132467+ShivanS93@users.noreply.github.com>
2023-10-05 22:36:49 +1300

    updating and removing yabai and skhd and limelight

--- fish/config.fish
+++ fish/config.fish
@@ -6,6 +6,7 @@ end
 alias brew="env PATH=(string replace (pyenv root)/shims '' \"\$PATH\") brew"
 status --is-interactive; and pyenv virtualenv-init - | source
 
+set -gx EDITOR nvim
 set -gx CLICOLOR 1
 set -gx LSCOLORS Exfxcxdxbxegedabagacad
 
@@ -13,11 +14,11 @@ fish_add_path /opt/homebrew/opt/node@16/bin
 fish_add_path /opt/homebrew/bin
 fish_add_path /$HOME/.local/bin
 fish_add_path /$HOME/.cargo/bin
+fish_add_path /opt/homebrew/opt/libpq/bin
 
-# >>> conda initialize >>>
-# !! Contents within this block are managed by 'conda init' !!
-eval /Users/shivan/miniconda3/bin/conda "shell.fish" hook $argv | source
-# <<< conda initialize <<<
+# psql
+set -gx LDFLAGS "-L/opt/homebrew/opt/libpq/lib"
+set -gx CPPFLAGS "-I/opt/homebrew/opt/libpq/include"
 
 # Python set up
 set -gx PYTHONDONTWRITEBYTECODE 1
@@ -25,31 +26,33 @@ set -gx DJANGO_DEVELOPMENT 1
 set -gx PIPENV_VENV_IN_PROJECT 1
 set -gx PYRIGHT_PYTHON_FORCE_VERSION latest
 
-# Flutter
-fish_add_path $HOME/flutter/bin
-
 # Go set up
 set -x GOPATH $HOME/Projects/go/
 fish_add_path /$GOPATH/bin
 alias go="grc go"
 alias godev="cd $HOME/Projects/go/src/github.com/shivan-s/"
 
-# kubectl
-alias kubectl="minikube kubectl --"
-
-# glow
-set -gx EDITOR nvim
-
-# set -gx TERM xterm-256color
-
 # Fish commands
-alias la="ls -alh"
 alias rm="rm -i"
 alias cp="cp -i"
 alias firefox="open -a firefox"
 alias vim="nvim"
-alias gi="git add . && git commit -m 'init' && git push"
-alias fishconfig="$EDITOR $HOME/.dotfiles/fish/config.fish"
+alias psql="pgcli"
+abbr --add t "tmux"
+abbr --add la "ls -alh"
+abbr --add ex "exa -a --icons -s modified --group-directories-first"
+abbr --add ea "exa -la --git -s modified -r --icons --header"
+abbr --add et "exa -T --git-ignore --icons"
+abbr --add gia "git add"
+abbr --add gic "git commit -m '"
+abbr --add gip "git pull"
+abbr --add gich "git checkout"
+abbr --add gil "git log --oneline"
+abbr --add gif "git fetch"
+abbr --add firefox "open -a firefox"
+abbr --add fishconfig "$EDITOR $HOME/.dotfiles/fish/config.fish"
+abbr --add fishso "source $HOME/.config/fish/config.fish"
+abbr --add vimconfig "$EDITOR $HOME/.dotfiles/nvim/"
 
 # sync dotfiles
 function dotfileupdate
@@ -70,7 +73,7 @@ function swapempty
 end
 
 # zk
-set -gx ZK_NOTEBOOK_DIR $HOME/Notes/
+set -gx ZK_NOTEBOOK_DIR $HOME/notes
 
 # pipenv completions
 eval (env _PIPENV_COMPLETE=fish_source pipenv)
@@ -78,21 +81,11 @@ eval (env _PIPENV_COMPLETE=fish_source pipenv)
 # FZF
 set -gx FZF_DEFAULT_OPTS "--layout=reverse --preview=bat"
 
-# starship
-# TODO: lookj into this
-# starship init fish | source
-
 # pnpm
-set -gx PNPM_HOME $HOME/Library/pnpm
+set -gx PNPM_HOME "$HOME/Library/pnpm"
 if not string match -q -- $PNPM_HOME $PATH
-    set -gx PATH "$PNPM_HOME" $PATH
+  set -gx PATH "$PNPM_HOME" $PATH
 end
-# pnpm end
-
-# Haskell
-set -q GHCUP_INSTALL_BASE_PREFIX[1]; or set GHCUP_INSTALL_BASE_PREFIX $HOME
-fish_add_path $HOME/.cabal/bin
-fish_add_path $HOME/.ghcup/bin
 
 # Racket
-fish_add_path /Applications/Racket/bin
+fish_add_path /Applications/Racket/bin/
