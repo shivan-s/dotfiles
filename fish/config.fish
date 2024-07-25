0b54d3f76e3f127fe2dbfb6765ace607d2a2937e
Shivan Sivakumaran <129010893+shivan-eyespace@users.noreply.github.com>
2024-07-26 09:41:31 +1200

    chore wip

--- fish/config.fish
+++ fish/config.fish
@@ -95,3 +95,17 @@ fish_add_path /Applications/Racket/bin/
 
 # flutter
 fish_add_path $HOME/development/flutter/bin/
+
+# >>> conda initialize >>>
+# !! Contents within this block are managed by 'conda init' !!
+if test -f /opt/homebrew/Caskroom/miniconda/base/bin/conda
+    eval /opt/homebrew/Caskroom/miniconda/base/bin/conda "shell.fish" "hook" $argv | source
+else
+    if test -f "/opt/homebrew/Caskroom/miniconda/base/etc/fish/conf.d/conda.fish"
+        . "/opt/homebrew/Caskroom/miniconda/base/etc/fish/conf.d/conda.fish"
+    else
+        set -x PATH "/opt/homebrew/Caskroom/miniconda/base/bin" $PATH
+    end
+end
+# <<< conda initialize <<<
+
