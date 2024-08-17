058cc807f5871d97604c509409e1a0054e97b029
Shivan Sivakumaran <me@shivan.xyz>
2024-08-17 18:27:59 +1200

    chore: fixing zemmode

--- fish/config.fish
+++ fish/config.fish
@@ -105,7 +105,15 @@ else
         . "/opt/homebrew/Caskroom/miniconda/base/etc/fish/conf.d/conda.fish"
     else
         set -x PATH "/opt/homebrew/Caskroom/miniconda/base/bin" $PATH
+if test -f /opt/miniconda3/bin/conda
+    eval /opt/miniconda3/bin/conda "shell.fish" "hook" $argv | source
+else
+    if test -f "/opt/miniconda3/etc/fish/conf.d/conda.fish"
+        . "/opt/miniconda3/etc/fish/conf.d/conda.fish"
+    else
+        set -x PATH "/opt/miniconda3/bin" $PATH
     end
 end
 # <<< conda initialize <<<
-
+# alr
+fish_add_path /usr/local/bin/alr
