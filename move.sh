4ba4dfba60dfc1091f94bc8bc5883a750369ee58
Shivan Sivakumaran <129010893+shivan-eyespace@users.noreply.github.com>
2024-10-03 09:19:30 +1300

    feat: adds newsboat

--- move.sh
+++ move.sh
@@ -1,15 +1,14 @@
 #!/usr/bin/env bash
 
-ln -sf $(pwd)/nvim $HOME/.config/nvim
+ln -sf $(pwd)/nvim $HOME/.config/
 ln -sf $(pwd)/alacritty $HOME/.config/
-ln -sf $(pwd)/fish $HOME/.config/fish
-ln -sf $(pwd)/omf $HOME/.config/omf
-ln -sf $(pwd)/.tmux.conf $HOME/.tmux.conf
-ln -sf $(pwd)/.tmux.conf.local $HOME/.tmux.conf.local
-ln -sf $(pwd)/sql-language-server $HOME/.config/sql-language-server
+ln -sf $(pwd)/newsboat $HOME/.config/
+ln -sf $(pwd)/fish $HOME/.config/
+ln -sf $(pwd)/omf $HOME/.config/
+ln -sf $(pwd)/.tmux.conf $HOME/
+ln -sf $(pwd)/.tmux.conf.local $HOME/
+ln -sf $(pwd)/sql-language-server $HOME/.config/
 
 mkdir $HOME/.zk
-ln -sf $(pwd)/zk/templates $HOME/.zk/templates
-ln -sf $(pwd)/zk/.config $HOME/.zk/.config
-
-git config --global push.autoSetupRemote true
+ln -sf $(pwd)/zk/templates $HOME/.zk/
+ln -sf $(pwd)/zk/.config $HOME/.zk/
