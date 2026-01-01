2a9cc57b42108b3e0b8ec34ba4360b13aec4592a
Shivan Sivakumaran <me@shivan.xyz>
2026-01-01 18:23:49 +1300

    chore: fish prompt

--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -1,46 +1,61 @@
-function fish_prompt
-    set -l last_status $status
+function fish_prompt --description 'Write out the prompt'
+    set -l last_pipestatus $pipestatus
+    set -lx __fish_last_status $status # Export for __fish_print_pipestatus.
 
-    set -l normal (set_color normal)
-    set -l usercolor (set_color $fish_color_user)
-
-    set -l delim \U25BA
-    # If we don't have unicode use a simpler delimiter
-    string match -qi "*.utf-8" -- $LANG $LC_CTYPE $LC_ALL; or set delim ">"
-
-    fish_is_root_user; and set delim "#"
-
-    set -l cwd (set_color $fish_color_cwd)
-    if command -sq sha256sum
-        # randomized cwd color
-        set -l shas (pwd -P | sha256sum | string sub -l 6 | string match -ra ..)
-        # Increase color a bit so we don't get super dark colors.
-        # Really we want some contrast to the background (assuming black).
-        set -l col (for f in $shas; math --base=hex "min(255, 0x$f + 0x30)"; end | string replace 0x '' | string pad -c 0 -w 2 | string join "")
-
-        set cwd (set_color $col)
+    if not set -q __fish_git_prompt_show_informative_status
+        set -g __fish_git_prompt_show_informative_status 1
+    end
+    if not set -q __fish_git_prompt_hide_untrackedfiles
+        set -g __fish_git_prompt_hide_untrackedfiles 1
+    end
+    if not set -q __fish_git_prompt_color_branch
+        set -g __fish_git_prompt_color_branch magenta --bold
+    end
+    if not set -q __fish_git_prompt_showupstream
+        set -g __fish_git_prompt_showupstream informative
+    end
+    if not set -q __fish_git_prompt_color_dirtystate
+        set -g __fish_git_prompt_color_dirtystate blue
+    end
+    if not set -q __fish_git_prompt_color_stagedstate
+        set -g __fish_git_prompt_color_stagedstate yellow
+    end
+    if not set -q __fish_git_prompt_color_invalidstate
+        set -g __fish_git_prompt_color_invalidstate red
+    end
+    if not set -q __fish_git_prompt_color_untrackedfiles
+        set -g __fish_git_prompt_color_untrackedfiles $fish_color_normal
+    end
+    if not set -q __fish_git_prompt_color_cleanstate
+        set -g __fish_git_prompt_color_cleanstate green --bold
     end
 
-    # Prompt status only if it's not 0
-    set -l prompt_status
-    test $last_status -ne 0; and set prompt_status (set_color $fish_color_error)"[$last_status]$normal"
-
-    # Only show host if in SSH or container
-    # Store this in a global variable because it's slow and unchanging
-    if not set -q prompt_host
-        set -g prompt_host ""
-        if set -q SSH_TTY
-            or begin
-                command -sq systemd-detect-virt
-                and systemd-detect-virt -q
-            end
-            set -l host (hostname)
-            set prompt_host $usercolor$USER$normal@(set_color $fish_color_host)$host$normal":"
+    set -l color_cwd
+    set -l suffix
+    if functions -q fish_is_root_user; and fish_is_root_user
+        if set -q fish_color_cwd_root
+            set color_cwd $fish_color_cwd_root
+        else
+            set color_cwd $fish_color_cwd
         end
+        set suffix '#'
+    else
+        set color_cwd $fish_color_cwd
+        set suffix '$'
     end
 
-    # Shorten pwd if prompt is too long
-    set -l pwd (prompt_pwd)
+    # PWD
+    set_color $color_cwd
+    echo -n (prompt_pwd)
+    set_color normal
 
-    echo -n -s $prompt_host $cwd $pwd $normal $prompt_status $delim
+    printf '%s ' (fish_vcs_prompt)
+
+    set -l status_color (set_color $fish_color_status)
+    set -l statusb_color (set_color --bold $fish_color_status)
+    set -l prompt_status (__fish_print_pipestatus "[" "]" "|" "$status_color" "$statusb_color" $last_pipestatus)
+    echo -n $prompt_status
+    set_color normal
+
+    echo -n "$suffix "
 end
